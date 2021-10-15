5c67ffdb9abaae6dbc38c47489ffc8b0cd88aa50
mindae <mahendrachaurasia@gmail.com>
2021-10-15 22:30:23 +0530

    first commit

---
+++ src/test/java/com/mindae/moviediscoveryserver2/MovieDiscoveryServer2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindae.moviediscoveryserver2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieDiscoveryServer2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
